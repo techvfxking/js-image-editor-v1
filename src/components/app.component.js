0c5487091b54f51b6f3d0173e59464f3cab6d295
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2025-04-26 13:47:09 +0530

    Committing changes for added app.component js

---
+++ src/components/app.component.js
@@ -0,0 +1,11 @@
+const AppRoot = () => {
+    const element = `
+        <main class="container">
+            <h2>Image Editor V1</h2>
+        </main>
+    `;
+
+    return element
+};
+
+export default AppRoot;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import './style.css';
+import { getElement } from './utils/helper.js';
+import AppRoot from './components/app.component.js';
 
 const main = () => {
-    
+    const app = getElement("#app");
+    app.innerHTML = AppRoot();
 }
 
 main();
