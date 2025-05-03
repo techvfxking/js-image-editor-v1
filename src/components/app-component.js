bb3b51a84f28c22175df7bb6f54e0efce1cf1667
@SunayanaCyberSwift <sunayana.sadhukhan@cyberswift.com>
2025-05-03 12:15:26 +0530

    commit

---
+++ src/components/app-component.js
@@ -0,0 +1,11 @@
+const AppComponent = () => {
+    const element = `
+    <main class="container">
+    <h2>Image Editor</h2>
+    </main>
+    `;
+    return element;
+}
+
+
+export default AppComponent;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './style.css';
-
+import { getElement } from './utils/helper.js';
+import AppComponent,{} from './components/app-component.js'
 const main = () => {
-    
+    const app = getElement('#app');
+    app.innerHTML = AppComponent();
 }
 
 main();
